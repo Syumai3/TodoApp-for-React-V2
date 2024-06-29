23e6eb798ea35104e3a4bc6af689946172d8e1f0
s.umeda <syuhei19911212@gmail.com>
2024-06-29 11:50:45 +0900

    feat:選択したtodo のtitle を編集できる処理を実装

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,10 @@ function App() {
   const [newTodoTitle, setNewTodoTitle] = useState("");
   // 新しく追加する todo の id の状態
   const [todoId, setTodoId] = useState(3);
+  // 編集中の todo の id の状態
+  const [editingTodoId, setEditingTodoId] = useState<number | null>(null);
+  // 編集する todo のタイトルの状態
+  const [editTodoTitle, setEditTodoTitle] = useState("");
 
   // Todo の追加フィールドの入力をする関数
   const handleAddTodoInput = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -40,6 +44,36 @@ function App() {
     setTodos(filteredTodos);
   };
 
+  // 編集するtodoのtitleを入力する関数
+  const handleEditTodoInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEditTodoTitle(e.target.value);
+  };
+
+  // 編集したtodoを todoリストに反映させる処理
+  const handleEditTodo = (id: number) => {
+    setTodos(
+      // 既存のtodosをmapし、引数の id と一致した todo の title を、editTodoTitleに変更する処理
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, title: editTodoTitle } : todo
+      )
+    );
+    // 編集中のidをnullにする
+    setEditingTodoId(null);
+    // 編集中の title　も　空にする
+    setEditTodoTitle("");
+  };
+
+  // 編集ボタンを押下した時に、編集フォームを表示させる関数 (id と title を受け取って、editingTodoId と　editTodoTitle　の状態を変更する)
+  const handleOpenEditForm = (id: number, title: string) => {
+    setEditingTodoId(id);
+    setEditTodoTitle(title);
+  };
+
+  // キャンセルボタンを押下した時に、編集フォームを閉じる関数
+  const handleCloseEditForm = () => {
+    setEditingTodoId(null);
+  };
+
   return (
     <div style={{ paddingLeft: "10px" }}>
       <h1>Todoリスト</h1>
@@ -57,7 +91,16 @@ function App() {
         <FilterTodo />
       </div>
       <div>
-        <TodoList todos={todos} deleteTodo={handleDeleteTodo} />
+        <TodoList
+          todos={todos}
+          deleteTodo={handleDeleteTodo}
+          editingTodoId={editingTodoId}
+          editTodoTitle={editTodoTitle}
+          editTodoInput={handleEditTodoInput}
+          openEditForm={handleOpenEditForm}
+          closeEditForm={handleCloseEditForm}
+          editTodo={handleEditTodo}
+        />
       </div>
     </div>
   );
@@ -66,25 +109,53 @@ function App() {
 type TodoListProps = {
   todos: Todotype[];
   deleteTodo: (id: number) => void;
+  editingTodoId: number | null;
+  editTodoTitle: string;
+  editTodoInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  openEditForm: (id: number, title: string) => void;
+  closeEditForm: () => void;
+  editTodo: (id: number) => void;
 };
 // Todoリストを表示するコンポーネント
-function TodoList({ todos, deleteTodo }: TodoListProps) {
+function TodoList({
+  todos,
+  deleteTodo,
+  editingTodoId,
+  editTodoTitle,
+  editTodoInput,
+  openEditForm,
+  closeEditForm,
+  editTodo,
+}: TodoListProps) {
   return (
     <ul>
-      {todos.map((todo) => (
-        <li
-          key={todo.id}
-          style={{
-            display: "flex",
-            justifyContent: "flex-start",
-          }}
-        >
-          <span style={{ marginRight: "10px" }}>{todo.status}</span>
-          <span style={{ marginRight: "10px" }}>{todo.title}</span>
-          <button>編集</button>
-          <button onClick={() => deleteTodo(todo.id)}>削除</button>
-        </li>
-      ))}
+      {todos.map((todo) =>
+        // editingTodoId が存在する (todo の idと一致する)場合に、以下の 編集用のフォームを表示させる処理
+        editingTodoId === todo.id ? (
+          <li>
+            <input type="text" value={editTodoTitle} onChange={editTodoInput} />
+            {/* handleEditTodo関数の処理を実行 */}
+            <button onClick={() => editTodo(todo.id)}>完了</button>
+            <button onClick={closeEditForm}>キャンセル</button>
+          </li>
+        ) : (
+          <li
+            key={todo.id}
+            style={{
+              display: "flex",
+              justifyContent: "flex-start",
+            }}
+          >
+            <span style={{ marginRight: "10px" }}>{todo.status}</span>
+            <span style={{ marginRight: "10px" }}>{todo.title}</span>
+            {/* handleOpenEditForm の処理を実行 */}
+            <button onClick={() => openEditForm(todo.id, todo.title)}>
+              編集
+            </button>
+            <button onClick={() => deleteTodo(todo.id)}>削除</button>
+          </li>
+        )
+      )}
     </ul>
   );
 }
